e4b5ad834051f8085eb5c086b6359ba6c6341c28
norgeous <daniel@danielharte.co.uk>
2015-10-06 13:45:59 +0100

    jslint main file

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # crc32hex
 A javascript CRC32 that returns as hex, rather than a 32bit unsigned int
+
+* return values match php's crc() function, 
+* an instance of this object will cache crc operations for performance
+
+## useage
+
+module is supports AMD (requirejs), CommonJS (npm) and Globals export modes.
+
+### AMD
+coming soon
+
+### CommonJS
+coming soon
+
+### Globals
+coming soon
+
+#### disable caching
+
+If you are doing thousands of different crc operations on strings that you are certain will be different each time, you will want to disable cacheing
+
+```js
+crc32hex.config( {useCache:false} );
+```
--- crc32hex.js
+++ crc32hex.js
@@ -1,17 +1,62 @@
-define(['js-crc32'],function (crc32) {
+define(['js-crc32'], function (crc32) {
+    "use strict";
     return {
+
+        options: {
+            useCache: true
+        },
         crcCache: {},
-        str: function (str = "") {
+
+        config: function (options) {
+            options = options || {};
+
+            //merge incoming options and this.options, but does not 'merge' subkeys
+            if (Object.keys(options).length !== 0) {
+                //for(var k in options) {
+                //    this.options[k] = options[k];
+                //}
+                Object.keys(options).forEach(function (k) {
+                    this.options[k] = options[k];
+                });
+            }
+
+        },
+
+        str: function (str) {
+            str = str || "";
+
+            //force input to be string
+            str = str.toString();
+
             // check crc already cached
-            if(this.crcCache[str] !== undefined) var crc = this.crcCache[str];
-            else {
-                var crc = crc32.str(str);                    //get crc from sheetjs crc32
-                if (crc < 0) crc += 4294967296;              //convert unsigned 32-bit integer to signed?
-                crc = crc.toString(16).toUpperCase();        //convert to hex and uppercase
-                crc = ( "00000000" + crc ).slice(-8);        //zero pad
-                this.crcCache[str] = crc;                    //cache
+            var crc;
+            if (this.crcCache[str] !== undefined) {
+                crc = this.crcCache[str];
+            } else {
+
+                //get crc from sheetjs crc32
+                crc = crc32.str(str);
+
+                //convert unsigned 32-bit integer
+                if (crc < 0) {
+                    crc += 4294967296;
+                }
+
+                //convert to hex and uppercase
+                crc = crc.toString(16).toUpperCase();
+
+                //zero pad
+                crc = ("00000000" + crc).slice(-8);
+
+                //cache
+                if (this.options.useCache) {
+                    this.crcCache[str] = crc;
+                }
+
             }
             return crc;
         }
+
     };
+
 });
